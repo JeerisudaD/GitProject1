0f5b331b1a4e1579661afddeef1e5f536d631f8f
JeerisudaD <jeerisudadumpathik@gmail.com>
2022-05-14 12:40:11 -0400

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I am Groot!!");
+        System.out.println("I am Gigi!!!");
+
+        System.out.println("I am a correct code");
+        System.out.println("I am a correct code");
     }
 }
